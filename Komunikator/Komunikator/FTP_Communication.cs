d8be8fb917383d8ef29ff52141990350183ea3e5
Michal N <michal.niewolik@gmail.com>
2016-12-06 22:08:19 +0100

    Dodanie projektu z metodą UploadFileToFtp oraz klasą Testy

---
+++ Komunikator/Komunikator/FTP_Communication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Komunikator
+{
+    public class FTP_Communication
+    {
+        public void UploadFileToFtp(string url, string filePath, string username, string password)
+        {
+            var fileName = Path.GetFileName(filePath);
+            var request = (FtpWebRequest)WebRequest.Create(url + fileName);
+
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(username, password);
+            request.UsePassive = true;
+            request.UseBinary = true;
+            request.KeepAlive = false;
+
+            using (var fileStream = File.OpenRead(filePath))
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    fileStream.CopyTo(requestStream);
+                    requestStream.Close();
+                }
+            }
+
+            var response = (FtpWebResponse)request.GetResponse();
+            Console.WriteLine("Dodano na FTP: {0}", response.StatusDescription);
+            response.Close();
+        }
+
+    }
+}
---
+++ Komunikator/Komunikator/Testy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komunikator
+{
+    public class Testy
+    {
+        static void Test1_Ftp_Upload()
+        {
+            FTP_Communication test = new FTP_Communication();
+            //test.UploadFileToFtp("ftp://sino.prv.pl/", "sciezka do pliku", "login", "password");
+            test.UploadFileToFtp("ftp://sino.cba.pl/sino.cba.pl/", "sciezka do pliku", "login", "password");
+        }
+
+
+
+        public static void StartTest()
+        {
+            Test1_Ftp_Upload();
+
+        }
+
+    }
+}
