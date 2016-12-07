33c691644721aa57aeed7d8e6b660d6a51e80310
mateusz.lindel <mateusz.lindel@smcebi.edu.pl>
2016-12-07 19:14:04 +0100

    Dodanie klasy ConnectDataBase - lacznosc z baza danych oracle

---
+++ Komunikator/Komunikator/ConnectDataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+
+
+namespace Komunikator
+{
+    class ConnectDataBase
+    {
+        private static string GetConnectionString(string host, int port, string sid, string user, string password)
+        {
+            string conString = "Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = "
+                 + host + ")(PORT = " + port + "))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = "
+                 + sid + ")));Password=" + password + ";User ID=" + user;
+            return conString;
+        }
+        public static OracleConnection GetConnect(string host, int port, string sid, string user, string password)
+        {
+            OracleConnection oraConn = new OracleConnection();
+            oraConn.ConnectionString = GetConnectionString(host, port, sid, user, password);
+
+            return oraConn;
+        }
+    }
+}
--- Komunikator/Komunikator/Testy.cs
+++ Komunikator/Komunikator/Testy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
 
 namespace Komunikator
 {
@@ -14,12 +15,35 @@ namespace Komunikator
             //test.UploadFileToFtp("ftp://sino.prv.pl/", "sciezka do pliku", "login", "password");
             test.UploadFileToFtp("ftp://sino.cba.pl/sino.cba.pl/", "sciezka do pliku", "login", "password");
         }
+        static void Test_DB_connect()
+        {
+            string host = "";
+            int port = 0;
+            string sid = "";
+            string user = "";
+            string password = "";
+
+            Console.WriteLine("Proba");
+            OracleConnection con = ConnectDataBase.GetConnect(host, port, sid, user, password);
+            Console.WriteLine("Lacze sie z: " + con);
+            try
+            {
+                con.Open();
+                Console.WriteLine("Polaczono");
+            }
+            catch (Exception msg)
+            {
+                Console.WriteLine("Blad, ", msg);
+            }
+    
+        }
 
 
 
         public static void StartTest()
         {
-            Test1_Ftp_Upload();
+            //Test1_Ftp_Upload();
+            Test_DB_connect();
 
         }
 
